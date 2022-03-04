6836d3f3e106ddedf280f44bcf0524bae685d3e7
APOLLO08 <99278749+APOLLO08@users.noreply.github.com>
2022-03-05 01:24:58 +0700

    Delete appupdater.php

--- appupdater.php
+++
@@ -1,1 +0,0 @@
-{ "message":"รายละเอียดการอัปเดต\n\n= แก้ไข SpinUtil ให้ใช้งานง่ายขึ้น\n=เพิ่ม inputType เพื่อป้องกัน \n \nเราจะพัฒนาแอพพลิเคชั่นต่อไปให้ดีและเร็วกว่านี้❤️", "title":"อัปเดตแอพพลิเคชั่นเวอร์ชั่น 1.0.4", "link":"play.google.com/store/apps/details?id=com.clover.apollo", "version": 3 }
